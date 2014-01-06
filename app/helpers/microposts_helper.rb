9929d6a58809658654a9587767f63063a2c4fa5c
Tim Woodbury <tim@midpackrunner.com>
2014-01-06 12:49:13 -0500

    Fix word wrap helper issues with long strings as link anchor text

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,10 +1,20 @@
 module MicropostsHelper
 
   def wrap(content)
-    sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
+    sanitize(raw(content.split.map{ |s| evaluate_and_wrap_string(s) }.join(' ')))
   end
 
 private
+    def evaluate_and_wrap_string(text)
+      regex = /(.*)(href=["\']+.*["\']+)(.*)/
+      if text =~ regex
+        regex.match(text)
+        [wrap_long_string($1), $2, wrap_long_string($3)].join()
+      else
+        wrap_long_string(text)
+      end
+    end
+    
     def wrap_long_string(text, max_width = 30)
       zero_width_space = "&#8203;"
       regex = /.{1,#{max_width}}/
